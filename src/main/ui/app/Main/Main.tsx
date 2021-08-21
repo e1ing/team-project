8b05f5d2f330ea363ef8f49f8212ebb2535adc2f
Dima-Karpov <dimka.carpov2013@yandex.ru>
2021-08-21 09:12:39 +0300

    'main'

--- src/main/ui/app/Main/Main.tsx
+++ src/main/ui/app/Main/Main.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Header } from "../../Header/Header";
+import { Routes } from "../../routes/Routes";
 
 export const Main = () => {
     return (
         <div>
             <Header/>
+            <Routes/>
         </div>
     )
 }
