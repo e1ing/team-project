717ab5196cfc947f4283edb95b2b77e4f57cec25
Dima-Karpov <dimka.carpov2013@yandex.ru>
2021-08-21 08:52:43 +0300

    'app-launch'

--- src/main/ui/Header/Header.tsx
+++ src/main/ui/Header/Header.tsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 type HeaderPropsType = {}
 
-const Header: FC<HeaderPropsType> = ({}) => {
+export const Header: FC<HeaderPropsType> = ({}) => {
     return (
         <header>
             <div>
--- src/main/ui/app/App.tsx
+++ src/main/ui/app/App.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import './App.css';
 import { Provider } from 'react-redux';
-import {HashRouter} from "react-router-dom";
-import {SuperComponents} from "../SuperComponents";
+import { HashRouter } from "react-router-dom";
+import { SuperComponents } from "../SuperComponents";
+import { store } from '../../bll/store';
+import { Main } from './Main/Main';
 
 function App() {
   return (
     <div className="App">
       <HashRouter basename={process.env.PUBLIC_URL}>
-          <SuperComponents/>
-       {/* <Provider>*/}
-      {/*<Main/>*/}
-       {/* </Provider>*/}
-        </HashRouter>
+        <SuperComponents />
+        <Provider store={store}>
+          <Main />
+        </Provider>
+      </HashRouter>
     </div>
   );
 }
