85caa6bf0aa33049360bc7684737cb07364806a2
jwasp <nigushaa@yandex.ru>
2021-08-22 00:39:04 +0300

    fixed bug in Routes

--- src/main/ui/routes/Header.tsx
+++ src/main/ui/routes/Header.tsx
@@ -8,7 +8,6 @@ function Header() {
             <NavLink to={PATH.SIGN_UP}>sign Up</NavLink>
             <NavLink to={PATH.LOGIN}>login</NavLink>
             <NavLink to={PATH.PROFILE}>profile</NavLink>
-            <NavLink to={PATH.PASSWORD}>password</NavLink>
             <NavLink to={PATH.PASSWORD_CHANGE}>new password</NavLink>
             <NavLink to={PATH.SUPER_COMPONENTS}>superComponents</NavLink>
         </div>
--- src/main/ui/routes/Routes.tsx
+++ src/main/ui/routes/Routes.tsx
@@ -13,11 +13,11 @@ import {SuperComponents} from "../SuperComponents";
 export const PATH = {
     LOGIN: "/login",
     PROFILE: "/profile",
-    PASSWORD: "/password",
     PASSWORD_CHANGE: "/password-change",
     SIGN_UP: "/sign-up",
     SUPER_COMPONENTS: "/superComponents",
-    PASSWORD_RECOVERY: "/password-recovery"
+    PASSWORD_RECOVERY: "/password-recovery",
+    ERROR_404: "/404"
     // add paths
 }
 export const Routes = () => {
@@ -30,10 +30,10 @@ export const Routes = () => {
                 <Route path={PATH.PROFILE} render={() => <Profile/>}/>
                 <Route path={PATH.PASSWORD_RECOVERY} render={() => <PasswordRecovery/>}/>
                 <Route path={PATH.PASSWORD_CHANGE} render={() => <PasswordChange/>}/>
-                <Route path={"/404"} render={() => <Error404/>}/>
+                <Route path={PATH.ERROR_404} render={() => <Error404/>}/>
+                <Route path={PATH.SUPER_COMPONENTS} render={() => <SuperComponents/>}/>
                 <Redirect from={"*"} to={"/404"}/>
 
-                <Route path={PATH.SUPER_COMPONENTS} render={() => <SuperComponents/>}/>
             </Switch>
         </div>
     )
