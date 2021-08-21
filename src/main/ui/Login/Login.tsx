f7ebf6303249cd758ccf0d2cfae0bf34fde39a9e
jwasp <nigushaa@yandex.ru>
2021-08-22 00:52:02 +0300

    adding style, inputs, button to login component

--- src/main/ui/Login/Login.tsx
+++ src/main/ui/Login/Login.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import style from './Login.module.css'
+import {Button} from "../common/Button/Button";
 export const Login = () => {
     return (
-        <div style={{fontSize: "50px", textAlign: "center"}}>
+        <div className={style.container}>
             <div>login</div>
+            <input/>
+            <input/>
+            <Button>login</Button>
         </div>
     )
 }
