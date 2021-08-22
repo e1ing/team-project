839c382b6c5e78ad89227866edd194e4ce8e78c6
jwasp <nigushaa@yandex.ru>
2021-08-23 01:47:50 +0300

    adding formik

--- src/main/ui/Login/Login.tsx
+++ src/main/ui/Login/Login.tsx
@@ -3,7 +3,22 @@ import style from './Login.module.css'
 import {Button} from "../common/Button/Button";
 import {PATH} from "../routes/Routes";
 import {NavLink} from "react-router-dom";
+import {useFormik} from "formik";
+import Checkbox from "../common/Checkbox/Checkbox";
+
+
 export const Login = () => {
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+            rememberMe: false
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values));
+        },
+    })
+
     return (
         <div className={style.container}>
             <div className={style.form}>
@@ -11,10 +26,31 @@ export const Login = () => {
                     <h1>It-incubator</h1>
                     <h3>Sign In</h3>
                 </div>
-                <div className={style.input}>
-                    <input/>
-                    <input/>
-                </div>
+                <form className={style.input} onSubmit={formik.handleSubmit}>
+                    <label htmlFor="email">Email Address</label>
+                    <input
+                        id="email"
+                        name="email"
+                        type="email"
+                        onChange={formik.handleChange}
+                        value={formik.values.email}
+                    />
+                    <label htmlFor="password">Password</label>
+                    <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        onChange={formik.handleChange}
+                        value={formik.values.password}
+                    />
+                    <label htmlFor="rememberMe">Remember me</label>
+                    <Checkbox
+                        name="rememberMe"
+                        onChange={formik.handleChange}
+                        checked={formik.values.rememberMe}
+                    />
+
+                </form>
                 <NavLink to={PATH.PASSWORD_CHANGE} style={{textDecoration: "none"}}>Forgot Password</NavLink>
                 <div className={style.bottom}>
                     <button className={style.button}>
