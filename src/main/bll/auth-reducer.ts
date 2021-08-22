c577839c2efe4bbea302099ff6b6551b46b329d8
jwasp <nigushaa@yandex.ru>
2021-08-23 00:34:48 +0300

    adding aut-reducer. InitialState

--- src/main/bll/auth-reducer.ts
+++ src/main/bll/auth-reducer.ts
@@ -1,9 +1,34 @@
-
-
-type InitialStateType = typeof initialState
+//type InitialStateType = typeof initialState
 type ActionType = {}
 
-const initialState = {}
+type InitialStateType = {
+    email: string
+    _id: string
+    name: string
+    avatar?: string
+    publicCardPacksCount: number
+
+    created: number
+    updated: number
+    isAdmin: boolean
+    verified: boolean
+    rememberMe: boolean
+
+    error?: string
+}
+const initialState: InitialStateType  = {
+    email: "",
+    _id: "",
+    name: "",
+    avatar: "",
+    publicCardPacksCount: 0,
+
+    created: 0,
+    updated: 0,
+    isAdmin: false,
+    verified: false,
+    rememberMe: false
+}
 
 export const authReducer = (state: InitialStateType = initialState, action: ActionType):InitialStateType => {
     switch (action){
