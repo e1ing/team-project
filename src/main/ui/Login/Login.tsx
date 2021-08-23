9169cabb241a77988690ab5a3b20f72b2f666890
jwasp <nigushaa@yandex.ru>
2021-08-23 16:32:40 +0300

    refactoring with input and button

--- src/main/ui/Login/Login.tsx
+++ src/main/ui/Login/Login.tsx
@@ -1,22 +1,48 @@
 import React from 'react';
-import style from './Login.module.css'
-import {Button} from "../common/Button/Button";
+import style from './Login.module.css';
+import styleButton from './../common/Button/Button.module.css';
+import styleInput from './../common/Input/Input.module.css';
 import {PATH} from "../routes/Routes";
 import {NavLink} from "react-router-dom";
 import {useFormik} from "formik";
 import Checkbox from "../common/Checkbox/Checkbox";
+import {Input} from "../common/Input/Input";
+import {Button} from "../common/Button/Button";
 
 
 export const Login = () => {
+    type FormikErrorType = {
+        email?: string
+        password?: string
+        rememberMe?: boolean
+    }
+
     const formik = useFormik({
+
         initialValues: {
             email: '',
             password: '',
             rememberMe: false
         },
+        validate: (values) => {
+            const errors: FormikErrorType = {};
+            if (!values.email) {
+                errors.email = 'Required';
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = 'Invalid email address';
+            }
+            if(!values.password) {
+                errors.password = 'Incorrect password'
+            } else if(values.password.length < 3) {
+                errors.password = 'Please enter your password'
+            }
+            return errors;
+        },
         onSubmit: values => {
             alert(JSON.stringify(values));
+            formik.resetForm()
         },
+
     })
 
     return (
@@ -28,36 +54,30 @@ export const Login = () => {
                 </div>
                 <form className={style.input} onSubmit={formik.handleSubmit}>
                     <label htmlFor="email">Email Address</label>
-                    <input
-                        id="email"
-                        name="email"
-                        type="email"
-                        onChange={formik.handleChange}
-                        value={formik.values.email}
+                    <Input
+                        className={styleInput.input}
+                        {...formik.getFieldProps('email')}
                     />
+                    {formik.touched.email && formik.errors.email ? <div style={{color: 'red'}}>{formik.errors.email}</div> : null}
                     <label htmlFor="password">Password</label>
-                    <input
-                        id="password"
-                        name="password"
-                        type="password"
-                        onChange={formik.handleChange}
-                        value={formik.values.password}
+                    <Input
+                        className={styleInput.input}
+                        {...formik.getFieldProps('password')}
                     />
+                    {formik.touched.password && formik.errors.password ? <div style={{color: 'red'}}>{formik.errors.password}</div> : null}
                     <label htmlFor="rememberMe">Remember me</label>
                     <Checkbox
                         name="rememberMe"
                         onChange={formik.handleChange}
                         checked={formik.values.rememberMe}
-
                     />
-
                 </form>
                 <NavLink to={PATH.PASSWORD_CHANGE} style={{textDecoration: "none"}}>Forgot Password</NavLink>
                 <div className={style.bottom}>
                     <form onSubmit={formik.handleSubmit}>
-                        <button className={style.button}>
+                        <Button className={styleButton.button}>
                             login
-                        </button>
+                        </Button>
                     </form>
                     <div style={{color: "gray"}}>Don't have an account?</div>
                     <NavLink to={PATH.SIGN_UP} style={{color: "#232480", fontWeight: "bold", textDecoration: "none"}}>Sign
--- src/main/ui/app/App.tsx
+++ src/main/ui/app/App.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Provider } from 'react-redux';
-import { NavLink } from 'react-router-dom';
 import {HashRouter} from "react-router-dom";
-import {SuperComponents} from "../SuperComponents";
 import {Routes} from "../routes/Routes";
 import Header from "../Header/Header";
 
