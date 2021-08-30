105b3efa5ac44d1b8e8caef175d583f2a89b1fef
jwasp <nigushaa@yandex.ru>
2021-08-30 20:52:06 +0300

    refactoring with logout

--- src/main/ui/Profile/Profile.tsx
+++ src/main/ui/Profile/Profile.tsx
@@ -8,23 +8,16 @@ import {inspect} from "util";
 import styles from "./Profile.module.css"
 import {Redirect} from "react-router-dom";
 import {PATH} from "../routes/Routes";
+import {Logout} from "../Logout/Logout";
 
 export const Profile = () => {
-    const isLoggedIn = useSelector<AppRootStateType, boolean>(state => state.auth.isLoggedIn)
-    const dispatch = useDispatch()
-    const logoutHandler = useCallback(()=> {
-        dispatch(logoutTC())
-    }, [])
-    // if(!isLoggedIn) {
-    //     return <Redirect to={PATH.LOGIN}/>
 
     return (
         <div className={styles.block}>
             <div style={{fontSize: "50px", textAlign: "center"}}>
                 Profile
             </div>
-            {isLoggedIn &&
-            <Button className={styleButton.button} color={"inherit"} onClick={logoutHandler}>Logout</Button>}
+            <Logout/>
         </div>
     )
 }
