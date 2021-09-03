26b9f53c292ff00323f31525054c7b83dcad0920
e1ing <elvira.kisling@gmail.com>
2021-09-03 20:44:56 +0300

    useEffect un Search component

--- src/main/ui/common/Search/Search.tsx
+++ src/main/ui/common/Search/Search.tsx
@@ -1,30 +1,39 @@
-import React, {ChangeEvent, FC, useCallback, useEffect, useMemo, useState} from "react";
+import React, {ChangeEvent, FC, useEffect, useState} from "react";
 import style from "./Search.module.css"
-import debounce from 'lodash.debounce';
-import {setPackNameAC, setPacksTC} from "../../../bll/packs-reducer/packs-reduser";
-import {useDispatch} from "react-redux";
 
 type SearchPropsType = {
     names: string
 }
 
 export const Search: FC<SearchPropsType> = ({names}) => {
-    const dispatch = useDispatch();
+    /*const dispatch = useDispatch();*/
     const [searchValue, setSearchValue] = useState<string>("");
-    let changedName = names;
+
+    useEffect(() => {
+        const debounceFn = setTimeout(() => {
+            //dispatch
+        }, 2000)
+        return ()=>clearTimeout(debounceFn)
+    }, [setSearchValue])
+
+
+   /* let changedName = names;
     if (searchValue !== "") {
        changedName = names.toLowerCase();
-       }
-
+       }*/
+/*
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         setSearchValue(e.target.value);
         dispatch(setPackNameAC(searchValue));
         dispatch(setPacksTC())
     }
-
     const debouncedHandler = useMemo(() =>
         debounce(handleChange, 1000),
-        [ names, setSearchValue]);
+        [ names, setSearchValue]);*/
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearchValue(e.target.value);
+    }
 
     return (
         <input
@@ -32,7 +41,7 @@ export const Search: FC<SearchPropsType> = ({names}) => {
             name = {"search"}
             type={"text"}
             placeholder={"Searh packs"}
-            onChange ={debouncedHandler}
+            onChange ={handleChange}
         />
     )
 }
